6cf777d57f2ea6c1562525b5243810580d5293ae
chinthalasubhash <subhashchinthala7@gmail.com>
2025-06-20 10:06:34 +0530

    new file:   JAVASCRIPT/file1.js 	new file:   JAVASCRIPT/file2.js 	new file:   JAVASCRIPT/index.html

---
+++ JAVASCRIPT/file1.js
@@ -0,0 +1,18 @@
+let studentNames = "John";
+let age = 21;
+let promoted=true;
+let score;
+let city=null;
+// console.log(typeof studentNames); // string
+// console.log(typeof age); // number
+// console.log(typeof promoted); // boolean
+// console.log(typeof score); // undefined
+// console.log(typeof city); // object (null is considered an object in JavaScript)
+// let strAge = String(age); // Convert number to string
+// console.log(typeof strAge); // string
+// let rating = "5";
+// let strRating = Number(rating); // Convert string to number
+// console.log(typeof strRating); // number
+let flag = "true";
+let isFlag = Boolean(flag); // Convert string to boolean
+ console.log(typeof isFlag); // boolean
---
+++ JAVASCRIPT/file2.js
@@ -0,0 +1,24 @@
+// let a = 54;
+// let b = 10;
+// console.log(a + b); // Addition
+// console.log(a%b);
+
+// let a = "10";
+// let b = 20;
+// console.log(a + b); // Concatenation (string + number)
+// console.log(a - b); // Subtraction (string - number, string is converted to number)
+// console.log(a * b); // Multiplication (string * number, string is converted to number
+
+
+// let a ="2";
+// let b=2;
+// console.log(a==b); // Equality check (value and type)
+// console.log(a===b); // Strict equality check (value and type)
+
+let a =10;
+let b=2;
+let c=12;
+// console.log(a>b && a<c); // Logical AND (both conditions must be true)
+
+console.log(!a>b); // Logical NOT (negates the condition)
+console.log(a>b || a<c); // Logical OR (at least one condition must be true
