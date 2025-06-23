9b6f960d365c100f6137570b6abe2d6714c30fa0
chinthalasubhash <subhashchinthala7@gmail.com>
2025-06-23 10:12:58 +0530

    new file:   JAVASCRIPT/file10.js 	new file:   JAVASCRIPT/file9.js

---
+++ JAVASCRIPT/file10.js
@@ -0,0 +1,53 @@
+let cart = {};
+const products = [
+    {id : 1,name: "Product 1", price: 100},
+    {id : 2,name: "Product 2", price: 200},
+    {id : 3,name: "Product 3", price: 300},
+];
+// const showproduct  =  () =>
+// {
+//     products.forEach((product) => {
+//         console.log(`ID: ${product.id}, Name: ${product.name}, Price: $ ${product.price}`);
+//     });
+// }
+// showproduct();
+
+const addToCart = (id) => {
+    cart = {...cart, [id]: 1};
+    console.log(`Product with ID ${id} added to cart.`);
+    console.log("Current Cart:", cart);
+
+};
+addToCart(1);
+addToCart(2);
+
+// const showproduct = () => {
+//     products.forEach((product) => {
+//         console.log(`ID: ${product.id}, Name: ${product.name}, Price: $${product.price}`);
+//     });
+// }
+// showproduct();
+
+const showcart = () => {
+    if (Object.keys(cart).length === 0) {
+        console.log("Cart is empty.");
+        return;
+    }
+    console.log("Current Cart:");
+    for (const id in cart) {
+        const product = products.find(p => p.id == id);
+        if (product) {
+            console.log(`ID: ${product.id}, Name: ${product.name}, Price: $${product.price}`);
+        }
+    }
+ }
+ showcart();
+
+
+
+
+
+
+
+
+
---
+++ JAVASCRIPT/file9.js
@@ -0,0 +1,25 @@
+const points = [2,3,4,1,34,45,5];
+// const newarr = points.map((value,index,arr)=>
+// {
+//     // console.log(value,index,arr);
+//     return value * 2; // Example operation: doubling each value
+// })
+// console.log(newarr); // Output: [4, 6, 8, 2, 68, 90, 10]
+
+// const filteredArr = points.filter((value,index,arr)=>
+// {
+//     // console.log(value,index,arr);
+//     return value > 10; // Example condition: filtering values greater than 10
+// })
+// console.log(filteredArr); // Output: [34, 45]
+// const findValue = points.find((value,index,arr)=>
+// {
+//     // console.log(value,index,arr);
+//     return value > 10; // Example condition: finding the first value greater than 10
+// })
+// console.log(findValue); // Output: 34
+
+const newarr = points.reduce((accumulator, value, index, arr) => {
+    return accumulator + value; 
+}, 10); 
+console.log(newarr); 
